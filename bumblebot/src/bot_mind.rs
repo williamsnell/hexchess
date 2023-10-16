76cd02c79a938aa04ed83c168d6dc740e8d09b70
williamsnell <aussiesnells@gmail.com>
2023-10-16 21:00:52 +1300

    add alpha-beta pruning

---
+++ bumblebot/src/bot_mind.rs
@@ -0,0 +1,131 @@
+use std::{thread::current, cmp::{max, min}};
+
+use hexchesscore::{get_all_valid_moves, Board, Color, Move, Piece, PieceType};
+
+fn evaluate_board(board: &Board) -> i16 {
+    // count each player's pieces
+    let mut black_count = 0;
+    let mut white_count = 0;
+    for (_hex, piece) in &board.occupied_squares {
+        let value = match piece.piece_type {
+            PieceType::Pawn => 1,
+            PieceType::Rook => 4,
+            PieceType::Bishop => 3,
+            PieceType::Knight => 3,
+            PieceType::Queen => 9,
+            PieceType::King => 0,
+        };
+        match piece.color {
+            Color::White => white_count += value,
+            Color::Black => black_count += value,
+        }
+    }
+    white_count - black_count
+}
+
+fn apply_move(board: &mut Board, movement: Move) -> (&mut Board, Option<Piece>) {
+    // this function assumes the move is legal. The legality checking
+    // should have already happened in the move generation
+    let moving_piece = board
+        .occupied_squares
+        .remove(&movement.start_hex)
+        .expect("Piece wasn't present at start hex");
+    let taken_piece = board
+        .occupied_squares
+        .insert(movement.final_hex, moving_piece);
+    board.current_player.invert();
+    (board, taken_piece)
+}
+
+fn revert_move(
+    board: &mut Board,
+    movement: Move,
+    taken_piece: Option<Piece>,
+) -> (&mut Board, Option<Piece>) {
+    // this function assumes the move is legal. The legality checking
+    // should have already happened in the move generation
+    let moving_piece = board
+        .occupied_squares
+        .remove(&movement.final_hex)
+        .expect("Piece wasn't present at final hex");
+    board
+        .occupied_squares
+        .insert(movement.start_hex, moving_piece);
+    if let Some(taken_piece) = taken_piece {
+        board
+            .occupied_squares
+            .insert(movement.final_hex, taken_piece);
+    }
+    board.current_player.invert();
+    (board, taken_piece)
+}
+
+fn evaluate_move(board: &mut Board, movement: Move, color: Color) -> i16 {
+    let (new_board, taken_piece) = apply_move(board, movement);
+    let rating = evaluate_board(new_board);
+    revert_move(new_board, movement, taken_piece);
+
+    rating
+}
+
+pub fn minimax(board: &mut Board, movement: Move, depth: i8, mut alpha: i16, mut beta: i16) -> i16 {
+    let (new_board, taken_piece) = apply_move(board, movement);
+    let mut rating;
+    if depth == 0 {
+        rating = evaluate_board(board)
+            * (if matches!(board.current_player, Color::White) {
+                1
+            } else {
+                -1
+            })
+    } else {
+        // invert now, since we have to do it in either case
+        new_board.current_player.invert();
+        // if the current player is black, then we're white,
+        // and want to maximize the value
+        let moves = get_all_valid_moves(new_board);
+        // if there's a checkmate, it's the highest value possible
+        if moves.len() == 0 {
+            return i16::MIN
+        }
+        if matches!(new_board.current_player, Color::Black) {
+            rating = i16::MIN;
+            for valid_move in moves {
+                rating = minimax(new_board, valid_move, depth - 1, alpha, beta);
+                if rating > beta {
+                    break
+                }
+                alpha = max(alpha, rating);
+            }
+        // we're black, and want to minimize the value
+        } else {
+            rating = i16::MAX;
+            for valid_move in moves {
+                rating = minimax(new_board, valid_move, depth - 1, alpha, beta);
+                if rating < alpha {
+                    break
+                }
+                beta = min(beta, rating);
+            }
+        }
+    }
+    revert_move(board, movement, taken_piece);
+    rating
+}
+
+pub fn make_a_move(board: &mut Board, bot_color: Color) -> Move {
+    let move_options = get_all_valid_moves(board);
+    let mut best_move = move_options[0];
+    let mut best_move_rating = evaluate_move(board, best_move, bot_color);
+
+    for player_move in move_options {
+        let rating = minimax(board, player_move, 3, i16::MIN, i16::MAX);
+        dbg!(player_move, rating);
+        if rating > best_move_rating {
+            best_move_rating = rating;
+            best_move = player_move
+        }
+    }
+    best_move
+}
+
--- bumblebot/src/main.rs
+++ bumblebot/src/main.rs
@@ -1,7 +1,6 @@
 use std::{net::TcpStream, env};
 
-use hexchesscore::{get_all_valid_moves, Board, Color, Move, PieceType, Piece};
-use rand::{self, Rng};
+use hexchesscore::Color;
 
 use tungstenite::{connect, stream::MaybeTlsStream, Message, WebSocket};
 use url::Url;
@@ -11,6 +10,8 @@ use server::{
     session_handling::PlayerColor,
     websocket_messaging::{IncomingMessage, OutgoingMessage},
 };
+
+use bumblebot::bot_mind::make_a_move;
 
 fn match_player_color(color: PlayerColor) -> Color {
     match color {
@@ -43,7 +44,7 @@ fn handle_message(
         }
         OutgoingMessage::BoardState { mut board } => {
             if board.current_player == *current_color {
-                let intended_move = make_a_move(&mut board);
+                let intended_move = make_a_move(&mut board, *current_color);
                 let _ = socket.send(Message::Text(
                     serde_json::to_string(&IncomingMessage::RegisterMove {
                         user_id: user_id.to_string(),
@@ -88,71 +89,4 @@ fn main() {
         let msg = socket.read().expect("Error reading WS message");
         handle_message(msg, user_id, &mut current_color, &mut socket);
     }
-}
-
-fn evaluate_board(board: &Board) -> i16 {
-    // count each player's pieces
-    let mut black_count = 0;
-    let mut white_count = 0;
-    for (_hex, piece) in &board.occupied_squares {
-        let value = match piece.piece_type {
-            PieceType::Pawn => 1,
-            PieceType::Rook => 4,
-            PieceType::Bishop => 3, 
-            PieceType::Knight => 3, 
-            PieceType::Queen => 9,
-            PieceType::King => 0
-        };
-        match piece.color {
-            Color::White => {white_count += value},
-            Color::Black => {black_count += value}
-        }
-    }
-    white_count - black_count
-}
-
-fn apply_move(board: &mut Board, movement: Move) -> (&mut Board, Option<Piece>) {
-    // this function assumes the move is legal. The legality checking
-    // should have already happened in the move generation
-    let moving_piece = board.occupied_squares.remove(&movement.start_hex).expect("Piece wasn't present at start hex");
-    let taken_piece = board.occupied_squares.insert(movement.final_hex, moving_piece);
-    board.current_player.invert();
-    (board, taken_piece)
-}
-
-fn revert_move(board: &mut Board, movement: Move, taken_piece: Option<Piece>) -> (&mut Board, Option<Piece>) {
-    // this function assumes the move is legal. The legality checking
-    // should have already happened in the move generation
-    let moving_piece = board.occupied_squares.remove(&movement.final_hex).expect("Piece wasn't present at final hex");
-    board.occupied_squares.insert(movement.start_hex, moving_piece);
-    if let Some(taken_piece) = taken_piece {
-        board.occupied_squares.insert(movement.final_hex, taken_piece);
-    }
-    board.current_player.invert();
-    (board, taken_piece)
-}
-
-fn evaluate_move(board: &mut Board, movement: Move) -> i16 {
-    let (mut new_board, taken_piece) = apply_move(board, movement);
-    let rating = evaluate_board(new_board);
-    revert_move(new_board, movement, taken_piece);
-
-    rating
-}
-
-fn make_a_move(board: &mut Board) -> Move {
-    let move_options = get_all_valid_moves(board);
-    let mut best_move = move_options[0];
-    let mut best_move_rating = evaluate_move(board, best_move);
-
-
-    for player_move in move_options {
-        let rating = evaluate_move(board, player_move);
-        dbg!(player_move, rating);
-        if rating > best_move_rating {
-            best_move_rating = rating;
-            best_move = player_move
-        }
-    }
-    return best_move;
 }
