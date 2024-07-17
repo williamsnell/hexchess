11da4ac3569afded5274277d9bb2fae65ea8a887
wsnell <aussiesnells@gmail.com>
2024-07-17 18:51:32 +1200

    fix npm audit, add sleep in build script

--- frontend/build.sh
+++ frontend/build.sh
@@ -4,5 +4,7 @@ npm install
 npm run build
 # kill the existing process
 killall -9 node
+# sleep in case killall takes a while?
+sleep .5
 # start the new process
 node build/index.js > ./err_f.txt 2>&1 &
