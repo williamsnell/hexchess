0a38125ffcbb8b36a3faa33d2f4b337965786a21
wsnell <aussiesnells@gmail.com>
2024-07-18 08:17:09 +1200

    remove kill command since it broke docker container boot

--- frontend/build.sh
+++ frontend/build.sh
@@ -2,9 +2,5 @@
 
 npm install
 npm run build
-# kill the existing process
-killall -9 node
-# sleep in case killall takes a while?
-sleep .5
 # start the new process
 node build/index.js > ./err_f.txt 2>&1 &
