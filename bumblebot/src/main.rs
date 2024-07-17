b0725d477fab1f0082cf1dc3de6fb4b6cdd6259e
wsnell <aussiesnells@gmail.com>
2024-07-18 10:37:03 +1200

    reduce timeout for bumblebot from 100s to 2

--- bumblebot/src/main.rs
+++ bumblebot/src/main.rs
@@ -54,7 +54,7 @@ async fn handle_message(
         }
         OutgoingMessage::BoardState { mut board } => {
             if board.current_player == *current_color {
-                let intended_move = make_a_move(&mut board, 100_000);
+                let intended_move = make_a_move(&mut board, 2000);
                 let _ = socket.send(tungstenite::Message::Text(
                     serde_json::to_string(&IncomingMessage::RegisterMove {
                         user_id: user_id.to_string(),
